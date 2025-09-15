9c45f7409b0edb1442357f5c98a7ba927fb6590d
BryanARMatheus <bryanassis244@gmail.com>
2025-09-15 19:59:07 -0300

    fix: adds rest of the initial configuration

---
+++ src/main/java/br/gov/sp/fatec/Lab14sem2025/Lab14sem2025Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.Lab14sem2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab14sem2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab14sem2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/Lab14sem2025/Lab14sem2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.Lab14sem2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab14sem2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
