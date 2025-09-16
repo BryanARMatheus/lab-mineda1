bd075c24885208e194fa6b97460e71bab621993a
BryanARM <163171268+BryanARMatheus@users.noreply.github.com>
2025-09-15 21:04:39 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/spingbootlab1/Spingbootlab1Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.spingbootlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spingbootlab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spingbootlab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/spingbootlab1/Spingbootlab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.spingbootlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spingbootlab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
