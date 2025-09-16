26ae342d656b62f98acafd56f6726c28eef018f6
BryanARM <163171268+BryanARMatheus@users.noreply.github.com>
2025-09-16 22:39:10 +0000

    feat: adds anotacao

--- src/main/java/br/gov/sp/fatec/Lab14sem2025/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/Lab14sem2025/entity/Usuario.java
@@ -1,10 +1,22 @@
 package br.gov.sp.fatec.Lab14sem2025.entity;
+
+import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import br.gov.sp.fatec.Lab14sem2025.entity.Anotacao;
+import br.gov.sp.fatec.Lab14sem2025.entity.Autorizacao;
 
 @Entity
 @Table(name = "usr_usuario")
@@ -18,7 +30,18 @@ public class Usuario {
     private String nome;
 
     @Column(name = "usr_senha")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String senha;
+
+    @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<Anotacao> anotacoes;
+
+    @ManyToMany
+    @JoinTable(name = "uau_usuario_autorizacao",
+        joinColumns = {@JoinColumn(name = "usr_id")},
+        inverseJoinColumns = {@JoinColumn(name = "aut_id")})
+    private List<Autorizacao> autorizacoes;
 
     public Usuario() { }
         public Usuario(String nome, String senha) {
